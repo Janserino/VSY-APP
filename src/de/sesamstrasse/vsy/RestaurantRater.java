7c1928c0ebbede159f6596988bd8670c8da98dbe
Chazor <f.wagner@live.de>
2016-01-20 21:10:32 +0100

    update Rater

--- src/de/sesamstrasse/vsy/RestaurantRater.java
+++ src/de/sesamstrasse/vsy/RestaurantRater.java
@@ -12,7 +12,9 @@ import java.util.concurrent.ExecutionException;
 /**
  * Created by wagnerf on 19.01.2016.
  */
+
 public class RestaurantRater extends Activity {
+    static public long sel_id;
 
     private float rating;
 
@@ -25,11 +27,11 @@ public class RestaurantRater extends Activity {
         try{
             final RatingBar newRating = (RatingBar)findViewById(R.id.RatingBar1);
             rating = newRating.getRating();
-            ConnectionHandler.rateRestaurant(rating, selected.id);
+            ConnectionHandler.rateRestaurant(rating, sel_id);
             Toast.makeText(RestaurantRater.this, "Restaurant" + selected.name + "bewertet!");
         }
         catch (ExecutionException e) {
-            Toast.makeText(Hauptseite.this, "Fehler bei der Bewertung", Toast.LENGTH_SHORT).show();
+            Toast.makeText(RestaurantRater.this, "Fehler bei der Bewertung", Toast.LENGTH_SHORT).show();
         }
     }
 }
